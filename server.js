3bfca9a459433e9feefeb7d6b38c6d337f610c3d
PVDMIDO <pvdemails@gmail.com>
2025-07-13 00:55:03 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./db');
+const sequelize = require('./db');
 require('dotenv').config();
 
 const app = express();
@@ -11,15 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Test PostgreSQL connection
-db.connect()
-    .then(obj => {
-        console.log('PostgreSQL connected');
-        obj.done(); // release connection
-    })
-    .catch(error => {
-        console.error('PostgreSQL connection error:', error);
-    });
+// Connect to PostgreSQL via Sequelize
+sequelize.authenticate()
+    .then(() => console.log('PostgreSQL connected'))
+    .catch((err) => console.error('PostgreSQL connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
