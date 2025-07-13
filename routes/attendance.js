4f93bcade48c858e59d41671b4dbd55f3fcb1b2e
PVDMIDO <pvdemails@gmail.com>
2025-07-14 01:24:38 +0400

    Delete routes directory

--- routes/attendance.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { markAttendance } = require('../controllers/attendanceController');
-
-router.post('/', markAttendance);
-
-module.exports = router;
