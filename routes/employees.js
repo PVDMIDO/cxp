6b4785a157e354dafb8fe8500cc4b73964f5bca8
PVDMIDO <pvdemails@gmail.com>
2025-07-15 09:47:58 +0400

    Update employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getEmployees, addEmployee } = require('../controllers/employeeController');
+const { getEmployees, addEmployee, updateEmployee, deleteEmployee } = require('../controllers/employeeController');
 
 router.get('/', getEmployees);
 router.post('/', addEmployee);
+router.put('/:id', updateEmployee);
+router.delete('/:id', deleteEmployee);
 
 module.exports = router;
